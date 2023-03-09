6e24bc29e98c445d144a6c6607ffe02a9701c3d2
LVRodrigues <lvrodriguesline@gmail.com>
2023-03-09 16:12:47 -0300

    Button.

--- hello-gtk/src/main.rs
+++ hello-gtk/src/main.rs
@@ -1,4 +1,4 @@
-use gtk::{Application, glib, prelude::{ApplicationExtManual, ApplicationExt}, ApplicationWindow, traits::GtkWindowExt};
+use gtk::{Application, glib, prelude::{ApplicationExtManual, ApplicationExt}, ApplicationWindow, traits::{GtkWindowExt, ButtonExt}, Button};
 
 const APP_ID: &str = "io.github.lvrodrigues.hello-gtk";
 
@@ -10,9 +10,22 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
+    let button = Button::builder()
+        .label("Clique me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    button.connect_clicked(|b| {
+        b.set_label("Hello, Luciano");
+    });
+    
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Hello RUST with GTK 4")
+        .child(&button)
+        .width_request(300)
         .build();
     window.present();
 }
